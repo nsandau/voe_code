7cc9f2185bb5e07ae5fdc0eb0ce659d50fe2b1f8
nsandau <12936029+nsandau@users.noreply.github.com>
2021-07-12 12:40:08 +0200

    update slurm_job

--- slurm_job.sh
+++ slurm_job.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=voe      # create a short name for your job
+#SBATCH --nodes=1
 #SBATCH --ntasks=1               # total number of tasks across all nodes
 #SBATCH --cpus-per-task=64       # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --exclusive
 #SBATCH --mail-type=all          # send email on start, end and fault
 #SBATCH --mail-user=kmd592@ku.dk
 #SBATCH --partition=modi_short  # modi_devel 15min, modi_short 48h, modi_long 7d 
