93205a5574aba30b7594b13316d753efb4fe445b
nsandau <12936029+nsandau@users.noreply.github.com>
2021-07-12 10:05:05 +0200

    split run_all

---
+++ run_bin.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sbatch slurm_job.sh bin 1
+sbatch slurm_job.sh bin 2
+sbatch slurm_job.sh bin 3
+sbatch slurm_job.sh bin 4
+sbatch slurm_job.sh bin 5
+sbatch slurm_job.sh bin 6
+
---
+++ run_func.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sbatch slurm_job.sh func 1
+sbatch slurm_job.sh func 2
+sbatch slurm_job.sh func 3
+sbatch slurm_job.sh func 4
+sbatch slurm_job.sh func 5
+sbatch slurm_job.sh func 6
+
