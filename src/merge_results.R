7336fcd995c638e26d801a919b6a0e493dfa04c0
nsandau <12936029+nsandau@users.noreply.github.com>
2021-08-06 14:08:14 +0200

    update merge_results

--- src/merge_results.R
+++ src/merge_results.R
@@ -10,24 +10,30 @@ output_dir <- here("output")
 df_paths <- output_dir %>%
     list.files()
 
-
-pvals_merged <- df_paths %>%
-    str_subset("pvals_") %>%
-    str_subset("merged", negate = T) %>%
-    future_map_dfr(~ read_feather(file.path(output_dir, .x)))
-
-results_merged <- df_paths %>%
+results_dfs <- df_paths %>%
     str_subset("results_") %>%
-    str_subset("merged", negate = T) %>%
+    str_subset("merged", negate = T)
+
+
+cat("Files identified:", results_dfs, sep = " \n")
+
+results_merged <- results_dfs %>%
     future_map_dfr(~ read_feather(file.path(output_dir, .x)))
 
 write_feather(
     results_merged,
     here::here("output", str_c("results_", "merged", ".feather"))
 )
-write_feather(
-    pvals_merged,
-    here::here("output", str_c("pvals_", "merged", ".feather"))
-)
+
+
+# pvals_merged <- df_paths %>%
+#     str_subset("pvals_") %>%
+#     str_subset("merged", negate = T) %>%
+#     future_map_dfr(~ read_feather(file.path(output_dir, .x)))
+
+# write_feather(
+#     pvals_merged,
+#     here::here("output", str_c("pvals_", "merged", ".feather"))
+# )
 
 toc()
