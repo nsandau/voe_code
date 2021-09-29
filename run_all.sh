90b122d56deb9d829293881abddfcbd4c2c87052
nsandau <12936029+nsandau@users.noreply.github.com>
2021-09-29 13:58:40 +0200

    upd okike

--- run_all.sh
+++ run_all.sh
@@ -6,10 +6,12 @@
 ## NO PROTOCOL
 bash run_splits.sh bin none 15 # passende 
 
-## Handoll split
-bash run_splits.sh bin handoll 3 # untested. decreased from 5
+## HANDOL AND RCT split
+for protocol in handoll rct; do
+bash run_splits.sh bin $protocol 3 # untested. decreased from 5
+done
 
-## WITH PROTOCOLS
+## BEKS AND SKOU
 for protocol in beks skou; do
 bash run_splits.sh bin $protocol 1
 done
@@ -18,17 +20,19 @@ done
 ## NO PROTOCOL
 bash run_splits.sh func none 8 # passende
 
-## HANDOLL
-bash run_splits.sh func handoll 2 # untested. decreased from 5
+## HANDOLL AND RCT
+for protocol in handoll rct; do
+bash run_splits.sh func $protocol 2 # untested. decreased from 5
+done
 
-## WITH PROTOCOLS 
+## SKOU BEKS 
 for protocol in beks skou; do
 bash run_splits.sh func $protocol 1
 done
 
 # QOL 
 ## ALL # beks doesnt include qol
-for protocol in none handoll skou; do
+for protocol in none handoll skou rct; do
 bash run_splits.sh qol $protocol 1
 done
 
