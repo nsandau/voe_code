25d3fb33a9d7d47da0784c45aa58b4c0ff4876db
nsandau <12936029+nsandau@users.noreply.github.com>
2022-02-24 13:36:40 +0100

    update most disc job

--- run_most_disc.sh
+++ run_most_disc.sh
@@ -2,3 +2,6 @@
 sbatch slurm_job.sh func handoll 1 1 --most_disc
 sbatch slurm_job.sh func none 2 1 --most_disc
 sbatch slurm_job.sh func none 2 2 --most_disc
+sbatch slurm_job.sh bin none 1 1 --most_disc
+sbatch slurm_job.sh bin beks 1 1 --most_disc
+sbatch slurm_job.sh bin skou 1 1 --most_disc
